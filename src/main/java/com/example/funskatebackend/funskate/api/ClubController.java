b88d9458bcc8f23e836616a786ec0e70605bbbd0
FHallengreen <fred70p9@stud.kea.dk>
2023-03-30 13:01:00 +0200

    added a new service and endpoint

--- src/main/java/com/example/funskatebackend/funskate/api/ClubController.java
+++ src/main/java/com/example/funskatebackend/funskate/api/ClubController.java
@@ -2,6 +2,7 @@ package com.example.funskatebackend.funskate.api;
 
 import com.example.funskatebackend.funskate.dto.athlete.AthleteResponse;
 import com.example.funskatebackend.funskate.dto.club.ClubResponse;
+import com.example.funskatebackend.funskate.entity.Club;
 import com.example.funskatebackend.funskate.service.ClubService;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -29,6 +30,13 @@ public class ClubController {
     @GetMapping("/{id}")
     public ClubResponse getClubById(@PathVariable int id) {
         return clubService.getClubById(id, false, true);
+    }
+
+
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'USER')")
+    @GetMapping("/clubid")
+    public int getClubId() {
+        return clubService.clubId();
     }
 
     @PreAuthorize("hasAnyAuthority('ADMIN', 'USER')")
